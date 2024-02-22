e7d9c769651206bb125d99eeb5901dbbbcf98c09
Mauro <krs.mauro@gmail.com>
2024-02-22 09:56:27 -0500

    sets up the testing environment, adds scafold tests for all components and tests BoldText.tsx

--- __tests__/BoldText.test.tsx
+++ __tests__/BoldText.test.tsx
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 import BoldText from "@/components/BoldText";
 
 describe("BoldText", () => {
-  it("renders text in heading with correct size", () => {
+  it("renders text in heading", () => {
     render(<BoldText size="lg" text="Hello World" />);
 
     const heading = screen.getByRole("heading", { name: "Hello World" });
 
-    expect(heading).toMatch("Hello World");
+    expect(heading).toBeInTheDocument();
   });
 });
---
+++ __tests__/InputField.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+
+test("passes", () => {
+  // render component
+
+  expect(true).toBe(true);
+});
